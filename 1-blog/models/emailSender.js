9eb6a50c9643bc066de7c5a59edc4b43c0c2809d
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-05 16:17:01 +0100

    send html email

--- 1-blog/models/emailSender.js
+++ 1-blog/models/emailSender.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+
 const transporter = nodemailer.createTransport({
     host: "mail.coding-school.org",
     port: 465,
@@ -12,21 +14,25 @@ const transporter = nodemailer.createTransport({
     }
   });
 function sendEmail(name, email, subject, message, callback) {
-    const mailOption = {
-        from: "fbw8@coding-school.org",
-        to: "ahmad.osman@digitalcareerinstitute.org",
-        subject: "email from your website",
-        text: subject + '\n' + name + '\n' + email + '\n' + message
-    };
-    transporter.sendMail(mailOption, (error, info) => {
-        if(error){
-            console.log(error);
-            callback(false);
-        } else {
-            console.log(info);
-            callback(true)
-        }
+    ejs.renderFile(__dirname + '/email-template.ejs',{subject, name, email, message }, (err, text) => {
+        const mailOption = {
+            from: "fbw8@coding-school.org",
+            to: "ahmad.osman@digitalcareerinstitute.org",
+            subject: "email from your website",
+            html:  text
+        };
+        transporter.sendMail(mailOption, (error, info) => {
+            if(error){
+                console.log(error);
+                callback(false);
+            } else {
+                console.log(info);
+                callback(true)
+            }
+        })
     })
+
+    
 }
 module.exports = {
     sendEmail
