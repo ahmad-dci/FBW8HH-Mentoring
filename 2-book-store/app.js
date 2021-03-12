6c44bd4fb18090ad13cfdf31c231a8a2ea9a5d2b
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-12 16:12:49 +0100

    getting data server side done

--- 2-book-store/app.js
+++ 2-book-store/app.js
@@ -11,6 +11,10 @@ app.set('views', __dirname + '/views');
 
 const port = process.env.PORT || 3000;
 
+// add express middleware to let the app to be able to get 
+// posted data as json or using get or form
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -20,6 +24,16 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
+// create a rout to get register data
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    res.json(1);
+})
+
 app.listen(port, () => {
     console.log(`app is running on the port ${port}`);
 })
+
+
+// save posted signup data to database
+// send a confirmation email to the user show that sign success
